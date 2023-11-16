f02ff2211f6bf88a57020e6eb968da58e5b9a872
Hope G <119355967+Ho0pHo0p@users.noreply.github.com>
2023-11-16 16:05:17 -0500

    Navbar visbility

--- app.js
+++ app.js
@@ -8,14 +8,17 @@ const menu = document.querySelector('#menu'
 
 const menuDisplay = (e) => {
   navbar.classList.toggle('hidden');
-  menu.classList.toggle('.hidden')
+  menu.classList.toggle('hidden')
   if(navbar.classList.contains('hidden')){
+    console.log('hidden')
     navbar.style.visibility = 'hidden'
     navbar.style.opacity = '0'
+    navbar.style.transitionDelay = '.1s linear'
     menu.style.visibility = 'hidden'
     menu.style.opacity = '0'
     menu.style.transitionDelay = '0s'
-  }else{
+  } else {
+    console.log('not hidden')
     menu.style.visibility = 'visible'
     menu.style.opacity = '1'
     menu.style.transitionDelay = '.1s linear'
@@ -25,3 +28,4 @@ const menuDisplay = (e) => {
 }
 
 bars.addEventListener('click', menuDisplay)
+
