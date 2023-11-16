386f513723d8b86a28dcae8381d5385ee0ce0332
Hope G <119355967+Ho0pHo0p@users.noreply.github.com>
2023-11-16 15:11:41 -0500

    Navbar transition

---
+++ app.js
@@ -0,0 +1,27 @@
+// Navbar
+
+const bars = document.querySelector('#bars');
+const navbar = document.querySelector('#navbar');
+const menu = document.querySelector('#menu'
+);
+
+
+const menuDisplay = (e) => {
+  navbar.classList.toggle('hidden');
+  menu.classList.toggle('.hidden')
+  if(navbar.classList.contains('hidden')){
+    navbar.style.visibility = 'hidden'
+    navbar.style.opacity = '0'
+    menu.style.visibility = 'hidden'
+    menu.style.opacity = '0'
+    menu.style.transitionDelay = '0s'
+  }else{
+    menu.style.visibility = 'visible'
+    menu.style.opacity = '1'
+    menu.style.transitionDelay = '.1s linear'
+    navbar.style.visibility = 'visible'
+    navbar.style.opacity = '1'
+  }
+}
+
+bars.addEventListener('click', menuDisplay)
